b0c75263a0cd7bb0bd863b353ee3616a8fa25bac
Herve Mbilo <herve.mb@outlook.com>
2019-12-06 20:55:16 +0100

    récupération du projet ANC3-MiniProject-V1-A-Donner

---
+++ ANC3-MiniProject-V1-A-Donner/src/miniproject/InvalidTransferException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miniproject;
+
+/**
+ *
+ * @author lasynsec
+ */
+public class InvalidTransferException extends RuntimeException {
+
+    public InvalidTransferException(String message) {
+       super(message);
+    }
+    
+}
---
+++ ANC3-MiniProject-V1-A-Donner/src/miniproject/Model.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miniproject;
+
+import static java.nio.file.Files.lines;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.TreeSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.scene.control.ListView;
+
+/**
+ *
+ * @author 2207hembilo
+ */
+public class Model {
+    private final List<String> toDoList = new ArrayList<>();
+    private final List<String> doneList = new ArrayList<>();
+    
+    public List<String> getToDoList() {
+        return Collections.unmodifiableList(toDoList);
+    }
+
+    public List<String> getDoneList() {
+        return Collections.unmodifiableList(doneList);
+    }
+    
+    public void setDone(int index){
+        if(index >= 0 && index < this.doneList.size()){
+            this.doneList.add(this.toDoList.remove(index));
+        }else{
+            throw new InvalidTransferException("Not possible !");
+        }     
+    }
+    
+     public void setToDo(int index){
+        if(index >= 0 && index < this.toDoList.size()){
+            this.toDoList.add(this.doneList.remove(index));
+        }else{
+            throw new InvalidTransferException("Not possible !");
+        }
+    }
+   
+/*
+    private static void transfer(ListView<String> fromList, ListView<String> toList) {
+        final int idxSel = fromList.getSelectionModel().getSelectedIndex();
+        if (idxSel != -1) {
+            toList.getItems().add(fromList.getItems().remove(idxSel));
+        }
+    }
+   /*
+    private void addToDo(String text) {
+        if (text.length() > 2 && !toDoList.getItems().contains(text) && !doneList.getItems().contains(text)) {
+            toDoList.getItems().add(text);
+            addText.setText("");
+        }
+    }
+    */
+    public Model(List<String> t){
+        
+        for(int i =0; i < t.size(); ++i){
+            if(t.get(i).length() >= 3){
+                if(!toDoList.contains(t.get(i))){
+                    this.toDoList.add(t.get(i));
+                }
+            }
+        }       
+    }
+    
+    public Model(){}
+    
+    public String toString(){
+        String temp ="";
+        
+        for(int i = 0; i < this.toDoList.size();++i){
+            temp += toDoList.get(i);
+        }
+        return temp;
+    }
+}
