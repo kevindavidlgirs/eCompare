18ecc506b95e094464f122f6bf480e8473deb0eb
kevin girs <kevin.girs@outlook.com>
2020-02-01 11:22:10 +0100

    Facade implémentée

--- eCompare/src/File/SimpleFile.java
+++ eCompare/src/File/SimpleFile.java
@@ -5,6 +5,7 @@
  */
 package File;
 
+import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
@@ -15,7 +16,7 @@ import java.util.List;
  */
 public class SimpleFile extends File {
 
-    public SimpleFile(String name, LocalDateTime date, long size, String path) {
+    public SimpleFile(String name, LocalDateTime date, long size, Path path) {
         super(name, date, size, path);
     }
 
@@ -28,27 +29,53 @@ public class SimpleFile extends File {
     protected String displayFormat(int offset) {
         return super.displayFormat(offset) + getName()
                 + ((this.isDirectory()) ? " D " : " F ")
-                + getDate().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")) +""                
-                + getSize() +" "
-                + getStatus()+ "\n";
+                + getDate().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")) + " "
+                + getSize() + " "
+                + getStatus() + "\n";
     }
 
-    @Override
-    public void addFile(File f) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private boolean isSame(File f1) {
+        if (this.getDate().isEqual(f1.getDate())) {
+            return true;
+        }
+        return false;
     }
 
-    @Override
-    public List<File> getList() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private boolean isOlder(File f1) {
+        if (this.getDate().isBefore(f1.getDate())) {
+            return true;
+        } else {
+            return false;
+        }
     }
+
+    private boolean isNewer(File f1) {
+        if (this.getDate().isAfter(f1.getDate())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean isOrphan(List<File> ls) {
+        for (File f : ls) {
+            if (f.getName().compareTo(this.getName()) == 0
+                    && f.isDirectory() && this.isDirectory()
+                    || !f.isDirectory() && !this.isDirectory()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    
 
     @Override
     public void compare(File f) {
         if (!f.isDirectory()) {
             if (this.getName().compareTo(f.getName()) == 0) {
-                if (this.isSame(f)) {                  
-                   this.set_status(Status.SAME);
+                if (this.isSame(f)) {
+                    this.set_status(Status.SAME);
                     f.set_status(Status.SAME);
                 } else if (this.isNewer(f)) {
                     this.set_status(Status.NEWER);
@@ -59,5 +86,15 @@ public class SimpleFile extends File {
                 }
             }
         }
+    }
+    
+    @Override
+    public void addFile(File f) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<File> getList() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
