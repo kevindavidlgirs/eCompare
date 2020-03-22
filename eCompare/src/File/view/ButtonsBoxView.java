d62cb5e543fcebd06ba9c3a5a167c9ba3f74f4d1
Kevin David L. Girs <kevin.girs@outlook.com>
2020-03-22 21:48:54 +0100

    add : bold text for toggleButton after selected

--- eCompare/src/File/view/ButtonsBoxView.java
+++ eCompare/src/File/view/ButtonsBoxView.java
@@ -7,7 +7,6 @@ package File.view;
 
 import File.viewModel.ViewModel;
 import javafx.geometry.Insets;
-import javafx.scene.control.Button;
 import javafx.geometry.Pos;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
@@ -33,34 +32,98 @@ public class ButtonsBoxView extends HBox{
     public ButtonsBoxView(ViewModel vm){
         newer_right_button.setToggleGroup(newer_left_right);
         newer_left_button.setToggleGroup(newer_left_right);
+
+        //Devrait se retrouver dans le "cssView.css" avec une chose du genre -> .toggle-button:selected { ..... }
+        all_button.setStyle("-fx-font-weight: bold");
+        //Devrait se retrouver dans le "cssView.css" avec une chose du genre -> .toggle-button:selected { ..... }
+
         setSpacing(10);
         setPadding(new Insets(10));
         setAlignment(Pos.CENTER);
         setBinding(vm);
 
-        //Faire en sorte que les boutons soient gras lors de la selection if(button.isSelected())... ?
         all_button.setOnAction(e -> {
             vm.set_selected_items(all_button.getText(), all_button.isSelected());
+
+            //Devrait se retrouver dans le "cssView.css" avec une chose du genre -> .toggle-button:selected { ..... }
+            if(all_button.isSelected()){
+                all_button.setStyle("-fx-font-weight: bold");
+                newer_right_button.setStyle("-fx-font-weight: regular");
+                newer_left_button.setStyle("-fx-font-weight: regular");
+                orphans_button.setStyle("-fx-font-weight: regular");
+                same_button.setStyle("-fx-font-weight: regular");
+                folders_only.setStyle("-fx-font-weight: regular");
+            } else {
+                all_button.setStyle("-fx-font-weight: regular");
+            }
+            //Devrait se retrouver dans le "cssView.css" avec une chose du genre -> .toggle-button:selected { ..... }
         });
         
         newer_right_button.setOnAction(e -> {
             vm.set_selected_items(newer_right_button.getText(), newer_right_button.isSelected());
+
+            //Devrait se retrouver dans le "cssView.css" avec une chose du genre -> .toggle-button:selected { ..... }
+            if(newer_right_button.isSelected()){
+                newer_right_button.setStyle("-fx-font-weight: bold");
+                newer_left_button.setStyle("-fx-font-weight: regular");
+                all_button.setStyle("-fx-font-weight: regular");
+            } else {
+                newer_right_button.setStyle("-fx-font-weight: regular");
+            }
+            //Devrait se retrouver dans le "cssView.css" avec une chose du genre -> .toggle-button:selected { ..... }
         });
         
         newer_left_button.setOnAction(e -> {
             vm.set_selected_items(newer_left_button.getText(), newer_left_button.isSelected());
-       });
+
+            //Devrait se retrouver dans le "cssView.css" avec une chose du genre -> .toggle-button:selected { ..... }
+            if(newer_left_button.isSelected()){
+                newer_left_button.setStyle("-fx-font-weight: bold");
+                newer_right_button.setStyle("-fx-font-weight: regular");
+                all_button.setStyle("-fx-font-weight: regular");
+            } else {
+                newer_left_button.setStyle("-fx-font-weight: regular");
+            }
+            //Devrait se retrouver dans le "cssView.css" avec une chose du genre -> .toggle-button:selected { ..... }
+        });
         
         orphans_button.setOnAction(e -> {
             vm.set_selected_items(orphans_button.getText(), orphans_button.isSelected());
+
+            //Devrait se retrouver dans le "cssView.css" avec une chose du genre -> .toggle-button:selected { ..... }
+            if(orphans_button.isSelected()){
+                orphans_button.setStyle("-fx-font-weight: bold");
+                all_button.setStyle("-fx-font-weight: regular");
+            } else {
+                orphans_button.setStyle("-fx-font-weight: regular");
+            }
+            //Devrait se retrouver dans le "cssView.css" avec une chose du genre -> .toggle-button:selected { ..... }
         });
         
         same_button.setOnAction(e -> {
             vm.set_selected_items(same_button.getText(), same_button.isSelected());
+
+            //Devrait se retrouver dans le "cssView.css" avec une chose du genre -> .toggle-button:selected { ..... }
+            if(same_button.isSelected()) {
+                same_button.setStyle("-fx-font-weight: bold");
+                all_button.setStyle("-fx-font-weight: regular");
+            } else {
+                same_button.setStyle("-fx-font-weight: regular");
+            }
+            //Devrait se retrouver dans le "cssView.css" avec une chose du genre -> .toggle-button:selected { ..... }
         });
         
         folders_only.setOnAction(e -> {
             vm.set_selected_items(folders_only.getText(), folders_only.isSelected());
+
+            //Devrait se retrouver dans le "cssView.css" avec une chose du genre -> .toggle-button:selected { ..... }
+            if(folders_only.isSelected()) {
+                folders_only.setStyle("-fx-font-weight: bold");
+                all_button.setStyle("-fx-font-weight: regular");
+            } else {
+                folders_only.setStyle("-fx-font-weight: regular");
+            }
+            //Devrait se retrouver dans le "cssView.css" avec une chose du genre -> .toggle-button:selected { ..... }
         });
     }
 
